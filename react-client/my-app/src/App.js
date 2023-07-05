255a86175601323e9b395565d73ff39a6150e196
Ian <ian.d.olmstead@gmail.com>
2023-07-04 23:38:23 -0500

    added dummy pages, navbar, and layout

--- react-client/my-app/src/App.js
+++ react-client/my-app/src/App.js
@@ -1,19 +1,29 @@
-import './App.css';
-import Layout from './components/layout.js'
+import Layout from './components/layout';
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+import Blogs from "./pages/Blog"
+import Contact from "./pages/Contact"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Layout>
-      <div className="App">
-        <header className="App-header">
+      <Layout>
+        <div className="App">
+          <header className="App-header">
 
-        </header>
-        <body>
-
-          <div class="text-center text-lg">Hello World</div>
-        </body>
-      </div>
-    </Layout>
+          </header>
+          <body>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<NoPage />} />
+            </Routes>
+          </BrowserRouter>
+          </body>
+        </div>
+      </Layout>
   );
 }
 
